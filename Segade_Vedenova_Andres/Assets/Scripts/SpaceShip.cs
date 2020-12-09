79d870aa5f30f1d1c3d18f809a058552e878997e
Sparkyx8 <72887115+Sparkyx8@users.noreply.github.com>
2020-12-09 18:11:43 +0100

    Movimiento de la nave horizontal y hacia delante
    Creado el movimiento de la nave en los ejes X y Z restringiendo el movimiento hacia atras

---
+++ Segade_Vedenova_Andres/Assets/Scripts/SpaceShip.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceShip : MonoBehaviour
+{
+    float speed = 10f;
+    bool forward = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveSpaceship();
+    }
+
+    public void moveSpaceship()
+    {
+        float myPosX = transform.position.x;
+        float desplX = Input.GetAxis("Horizontal");
+        float myPosZ = transform.position.z;
+        float desplY = Input.GetAxis("Vertical");
+        if (forward == true)
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed * desplY);
+        }
+        if (desplY <= 0)
+        {
+            forward = false;
+        }
+        else if (desplY > 0)
+        {
+            forward = true;
+        }
+        print(forward);
+        print(desplY);
+        transform.Translate(Vector3.right * Time.deltaTime * speed * desplX);
+        
+    }
+}
